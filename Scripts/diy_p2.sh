f9f480a4c98ab6153dbb7cb22ea97aa903a862c5
legna05 <1062723226@qq.com>
2026-03-07 17:53:00 +0800

    main

--- Scripts/diy_p2.sh
+++ Scripts/diy_p2.sh
@@ -6,4 +6,5 @@ $feeds_path install -a -f -p passwall_packages
 $feeds_path install -a -f -p passwall_luci
 $feeds_path install -a -f -p openclash
 $feeds_path install -a -f -p netspeedtest
+$feeds_path install -a -f -p turboacc
 $feeds_path install -a -f -p ddns_go
