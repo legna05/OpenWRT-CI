9d3fce752c081a8948613cd63102e8cf96e554e0
Administrator <106@11.com>
2026-06-05 05:26:39 +0800

    dg3399适配

--- Scripts/dg3399.sh
+++ Scripts/dg3399.sh
@@ -31,7 +31,7 @@ define Device/rockchip_dg3399
   DEVICE_MODEL := DG3399
   UBOOT_DEVICE_NAME := dg3399-rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-ata-ahci kmod-rtl8821ae kmod-usb-net-rtl8152 wpad \
+  DEVICE_PACKAGES := kmod-ata-ahci kmod-rtl8821ae kmod-usb-net-rtl8152 wpad-openssl \
     brcmfmac-nvram-43455-sdio cypress-firmware-43455-sdio
 endef
 TARGET_DEVICES += rockchip_dg3399' >> $WRT_PATH/target/linux/rockchip/image/armv8.mk
