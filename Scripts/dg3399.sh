52a213ed241a1fe7b5ec599172c4029db05c7624
Administrator <106@11.com>
2026-06-04 23:02:29 +0800

    dg3399适配

--- Scripts/dg3399.sh
+++ Scripts/dg3399.sh
@@ -24,7 +24,15 @@ echo "=============================================================="
 cp -f $DTS_PATH/kernel-rockchip/rk3399-dg3399.dts            $WRT_PATH/target/linux/rockchip/files/arch/arm64/boot/dts/rockchip/rk3399-dg3399.dts
 echo "=============================================================="
 # linux/rockchip/image/armv8.mk添加dg3399设备型号
-echo "" >> $DTS_PATH/kernel-rockchip/armv8.mk
-echo "" >> $WRT_PATH/target/linux/rockchip/image/armv8.mk
-cat $DTS_PATH/kernel-rockchip/armv8.mk >> $WRT_PATH/target/linux/rockchip/image/armv8.mk
+echo -e '
+define Device/rockchip_dg3399
+  $(Device/rk3399)
+  DEVICE_VENDOR := Rockchip
+  DEVICE_MODEL := DG3399
+  UBOOT_DEVICE_NAME := dg3399-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-ata-ahci kmod-rtl8821ae kmod-usb-net-rtl8152 wpad \
+    brcmfmac-nvram-43455-sdio cypress-firmware-43455-sdio
+endef
+TARGET_DEVICES += rockchip_dg3399' >> $WRT_PATH/target/linux/rockchip/image/armv8.mk
 cat $WRT_PATH/target/linux/rockchip/image/armv8.mk
