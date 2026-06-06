816fc729ba2c3dd5ede30fca8c8c2d429fc620cb
Administrator <106@11.com>
2026-06-06 17:12:10 +0800

    dg3399适配

--- Scripts/dg3399.sh
+++ Scripts/dg3399.sh
@@ -31,7 +31,7 @@ define Device/rockchip_dg3399
   DEVICE_VENDOR := Rockchip
   DEVICE_MODEL := DG3399
   UBOOT_DEVICE_NAME := dg3399-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-bin | gzip | append-metadata
   DEVICE_PACKAGES := kmod-ata-ahci kmod-rtl8821ae kmod-usb-net-rtl8152 wpad-openssl \
     brcmfmac-nvram-43455-sdio cypress-firmware-43455-sdio
 endef
